8035d72d38e2016e88482f0b75f547c3ccad94b7
anaodoro <anaodoro@gmail.com>
2013-01-31 01:27:37 -0300

    AT3

--- v01/all/body/index.php
+++ v01/all/body/index.php
@@ -1,2 +1,11 @@
+<?php
+$result = mysql_query("SELECT id, nome, descricao, abreviacao, t_porpagina, url, tipo, categoria FROM boards WHERE tipo = '1' ORDER BY categoria ASC");
+$numero = 0;
+?>
 <body>
+<div id="top">
+    <div id="name-ib"><?php echo $config_geral['name'];?></div>
+    <div id="allboards-ib"><?php while ($row = mysql_fetch_array($result)){$numero = $numero+1;if($numero == 1){$coch = '[ ';}else{$coch = '';}if(isset($categoria)){}else{$categoria = $row['categoria'];}if($categoria != $row['categoria']){$coch = ' ] [ ';}if($coch == " ] [ " || $coch == "[ "){$barra = '';}else{$barra=' / ';}echo $barra.$coch.$row['url'];$categoria = $row['categoria'];
+}?> ]</div>
+</div>
 </body>
